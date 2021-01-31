2c69d286fced8c2871a7e17b6d2f560bf7202f51
Abasiono Udosen <abasionoudosen@Abasionos-MacBook-Pro.local>
2021-01-31 21:05:43 +0100

    app.js - update

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const cors = require('cors');
-const mongoose = require('mongoose');
-const express = require('express');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const express = require("express");
+
+const { MONGODB } = require("./config");
 
 // Local Imports
-const usersRoute = require('./routes/users-route');
-const groupsRoute = require('./routes/groups-route');
-const messagesRoute = require('./routes/messages-route');
-const bugsRoute = require('./routes/bugs-route');
+const usersRoute = require("./routes/users-route");
+const groupsRoute = require("./routes/groups-route");
+const messagesRoute = require("./routes/messages-route");
+const bugsRoute = require("./routes/bugs-route");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/users', usersRoute);
-app.use('/api/groups', groupsRoute);
-app.use('/api/messages', messagesRoute);
-app.use('/api/bugs', bugsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/groups", groupsRoute);
+app.use("/api/messages", messagesRoute);
+app.use("/api/bugs", bugsRoute);
 
 // Error Handler
 app.use((error, req, res, next) => {
-  console.log('An error occured:', error);
-  res.json({ message: error.message || 'An unknown error occured.', error: true });
+  console.log("An error occured:", error);
+  res.json({
+    message: error.message || "An unknown error occured.",
+    error: true,
+  });
 });
 
 // Socket.io
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 class User {
   constructor(uid, sid) {
@@ -40,47 +45,50 @@ class User {
 let userList = [];
 
 // Establish a connection
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   // New user
-  socket.on('new user', uid => {
+  socket.on("new user", (uid) => {
     userList.push(new User(uid, socket.id));
   });
 
   // Join group
-  socket.on('join group', (uid, gid) => {
+  socket.on("join group", (uid, gid) => {
     for (let i = 0; i < userList.length; i++) {
       if (socket.id === userList[i].sid) userList[i].gid = gid;
     }
   });
 
   // New group
-  socket.on('create group', (uid, title) => {
-    io.emit('fetch group');
+  socket.on("create group", (uid, title) => {
+    io.emit("fetch group");
   });
 
   // New message
-  socket.on('message', (uid, gid) => {
+  socket.on("message", (uid, gid) => {
     for (const user of userList) {
-      if (gid === user.gid) io.to(user.sid).emit('fetch messages', gid);
+      if (gid === user.gid) io.to(user.sid).emit("fetch messages", gid);
     }
   });
 
   // Close connection
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     for (let i = 0; i < userList.length; i++) {
       if (socket.id === userList[i].sid) userList.splice(i, 1);
     }
   });
 });
 
-console.log(process.env.MONGO_URI) 
 
 // Connect to DB && Start server
 mongoose
-  .connect('mongodb://localhost:27017/chatGroup', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .connect(
+    "mongodb+srv://abazudosen:FSXvUDgcMkHyb42@messaging.ryocr.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
   .then(() => {
+    console.log('mongodb connected');
     server.listen(process.env.PORT || 5000, () =>
       console.log(`Server up and running on port ${process.env.PORT || 5000}!`)
     );
   })
-  .catch(error => console.log('Could not start server: ', error));
+  .catch((error) => console.log("Could not start server: ", error));
---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+ MONGODB: 'mongodb+srv://abazudosen:FSXvUDgcMkHyb42@messaging.ryocr.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
